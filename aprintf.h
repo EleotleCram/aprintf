e93e823933d9cd0e499e72d82bd51af27f990225
Marcel Toele <EleotleCram@gmail.com>
2021-06-04 18:04:10 +0200

    aprintf.h: clang format

--- aprintf.h
+++ aprintf.h
@@ -1,4 +1,4 @@
-/* 
+/*
  * File:   aprintf.h
  * Author: mtoele
  *
@@ -8,40 +8,46 @@
 #pragma once
 
 int aprintf(char *str, ...) {
-	int i, j, count = 0;
+  int i, j, count = 0;
 
-	va_list argv;
-	va_start(argv, str);
-	for(i = 0, j = 0; str[i] != '\0'; i++) {
-		if (str[i] == '%') {
-			count++;
+  va_list argv;
+  va_start(argv, str);
+  for (i = 0, j = 0; str[i] != '\0'; i++) {
+    if (str[i] == '%') {
+      count++;
 
-			Serial.write(reinterpret_cast<const uint8_t*>(str+j), i-j);
+      Serial.write(reinterpret_cast<const uint8_t *>(str + j), i - j);
 
-			switch (str[++i]) {
-				case 'd': Serial.print(va_arg(argv, int));
-					break;
-				case 'l': Serial.print(va_arg(argv, long));
-					break;
-				case 'f': Serial.print(va_arg(argv, double));
-					break;
-				case 'c': Serial.print((char) va_arg(argv, int));
-					break;
-				case 's': Serial.print(va_arg(argv, char *));
-					break;
-				case '%': Serial.print("%%");
-					break;
-				default:;
-			};
+      switch (str[++i]) {
+        case 'd':
+          Serial.print(va_arg(argv, int));
+          break;
+        case 'l':
+          Serial.print(va_arg(argv, long));
+          break;
+        case 'f':
+          Serial.print(va_arg(argv, double));
+          break;
+        case 'c':
+          Serial.print((char)va_arg(argv, int));
+          break;
+        case 's':
+          Serial.print(va_arg(argv, char *));
+          break;
+        case '%':
+          Serial.print("%%");
+          break;
+        default:;
+      };
 
-			j = i+1;
-		}
-	};
-	va_end(argv);
+      j = i + 1;
+    }
+  };
+  va_end(argv);
 
-	if(i > j) {
-		Serial.write(reinterpret_cast<const uint8_t*>(str+j), i-j);
-	}
+  if (i > j) {
+    Serial.write(reinterpret_cast<const uint8_t *>(str + j), i - j);
+  }
 
-	return count;
+  return count;
 }
