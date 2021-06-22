b0d52c031e447f22046da4cbb81614a16c1bbac8
Marcel Toele <EleotleCram@gmail.com>
2021-06-22 16:30:14 +0200

    aprintf.*: Fixed includes; moved object code to .cpp file to make aprintf.h reusable across multiple compilation units

---
+++ aprintf.cpp
@@ -0,0 +1,53 @@
+#include <aprintf.h>
+
+int avprintf(char *str, va_list argv) {
+  int i, j, count = 0;
+
+  for (i = 0, j = 0; str[i] != '\0'; i++) {
+    if (str[i] == '%') {
+      count++;
+
+      Serial.write(reinterpret_cast<const uint8_t *>(str + j), i - j);
+
+      switch (str[++i]) {
+        case 'd':
+          Serial.print(va_arg(argv, int));
+          break;
+        case 'l':
+          Serial.print(va_arg(argv, long));
+          break;
+        case 'f':
+          Serial.print(va_arg(argv, double));
+          break;
+        case 'c':
+          Serial.print((char)va_arg(argv, int));
+          break;
+        case 's':
+          Serial.print(va_arg(argv, char *));
+          break;
+        case '%':
+          Serial.print("%%");
+          break;
+        default:;
+      };
+
+      j = i + 1;
+    }
+  };
+
+  if (i > j) {
+    Serial.write(reinterpret_cast<const uint8_t *>(str + j), i - j);
+  }
+
+  return count;
+}
+
+int aprintf(char *str, ...) {
+  va_list argv;
+
+  va_start(argv, str);
+  const int count = avprintf(str, argv);
+  va_end(argv);
+
+  return count;
+}
--- aprintf.h
+++ aprintf.h
@@ -7,54 +7,11 @@
 
 #pragma once
 
-int avprintf(char *str, va_list argv) {
-  int i, j, count = 0;
+// Standard includes
+#include <stdarg.h>
 
-  for (i = 0, j = 0; str[i] != '\0'; i++) {
-    if (str[i] == '%') {
-      count++;
+// Arduino/Platform includes
+#include <Arduino.h>
 
-      Serial.write(reinterpret_cast<const uint8_t *>(str + j), i - j);
-
-      switch (str[++i]) {
-        case 'd':
-          Serial.print(va_arg(argv, int));
-          break;
-        case 'l':
-          Serial.print(va_arg(argv, long));
-          break;
-        case 'f':
-          Serial.print(va_arg(argv, double));
-          break;
-        case 'c':
-          Serial.print((char)va_arg(argv, int));
-          break;
-        case 's':
-          Serial.print(va_arg(argv, char *));
-          break;
-        case '%':
-          Serial.print("%%");
-          break;
-        default:;
-      };
-
-      j = i + 1;
-    }
-  };
-
-  if (i > j) {
-    Serial.write(reinterpret_cast<const uint8_t *>(str + j), i - j);
-  }
-
-  return count;
-}
-
-int aprintf(char *str, ...) {
-  va_list argv;
-
-  va_start(argv, str);
-  const int count = avprintf(str, argv);
-  va_end(argv);
-
-  return count;
-}
+int avprintf(char *str, va_list argv);
+int aprintf(char *str, ...);
